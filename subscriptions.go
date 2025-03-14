80cdb82559cf48dde8eedbfa82131abd07bcc11d
byhashdong <byhashdong@protonmail.com>
2025-03-14 18:12:43 +0800

    add one missing update

--- subscriptions.go
+++ subscriptions.go
@@ -1,10 +1,11 @@
 package deribit
 
 import (
-	"github.com/frankrap/deribit-api/models"
-	"github.com/json-iterator/go"
 	"log"
 	"strings"
+
+	jsoniter "github.com/json-iterator/go"
+	"github.com/ronfi/deribit-api/models"
 )
 
 func (c *Client) subscriptionsProcess(event *Event) {
